e5064cfe1509c58f549482ac3325f4b6d8d6f411
Mikiyas Desalegn <99103627+Mikiyasdes@users.noreply.github.com>
2024-09-14 21:01:21 +0300

    Update footer.php

--- Online-Job-Portal-System/footer.php
+++ Online-Job-Portal-System/footer.php
@@ -3,7 +3,7 @@
         <div id="top" class="noprint"><p><span class="noscreen">Back on top</span> <a href="#header" title="Back on top ^">^<span></span></a></p></div>
         <hr class="noscreen" />
         
-        <p class="style1" id="createdby">Created by Our Team
+        <p class="style1" id="createdby">Created by Mikiyas
         </p>
         <p>Online-JOB.com</p>
 </div> <!-- /footer -->
